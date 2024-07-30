caeed208bc612e6292548c45749f773859161744
firefoxic <firefoxic.dev@gmail.com>
2024-07-30 10:23:14 +0300

    Bump `stylelint` to `16.8.0`

--- utils/testRule.js
+++ utils/testRule.js
@@ -1,30 +1,32 @@
 import { deepEqual } from "node:assert/strict"
-import { readFile } from "node:fs/promises"
 import { describe, it } from "node:test"
 
 import stylelint from "stylelint"
 
 /**
- * Executes a test for a specified linting rule using stylelint.
+ * Test the specified rule with its configuration from the `stylelint.config.js` file.
  *
- * @param {Object} test - Test parameters.
- * @param {string} test.description - The test description.
- * @param {string} [test.rule] - The name of the rule under test. If not specified, stylelint will use all the rules in the `.stylelintrc` file.
+ * @param {Object} test - Test run parameters.
+ * @param {string} test.description - The description of the test.
+ * @param {string} [test.rule] - The name of the rule under test. If not specified, stylelint will use all the rules in the `stylelint.config.js` file.
  * @param {Object} [test.plugin] - The plugin required for the rule under test.
  * @param {string} test.code - The code for the rule under test.
  * @param {Array} test.expectedWarnings - Full data of the warnings expected in the test.
- * @returns {Promise<void>} A promise that resolves when the test is complete.
+ * @returns {Promise} A promise that resolves when the test is complete.
  */
 export async function testRule ({ description, rule, plugin, code, expectedWarnings }) {
-	if (!Array.isArray(expectedWarnings)) throw new TypeError(`expectedWarnings must be an array`)
-
-	const pluginName = plugin?.name
-	const config = await getConfig(rule, pluginName)
+	let config = rule
+		? {
+			plugins: plugin?.name,
+			rules: {
+				[rule]: await getRuleConfig(rule),
+			},
+		}
+		: undefined
 
 	describe(rule, () => {
 		it(description, async () => {
-			const result = await stylelint.lint({ code, config })
-			const warnings = result.results[0].warnings
+			let warnings = await stylelint.lint({ code, config }).then((r) => r.results[0].warnings)
 
 			deepEqual(warnings, expectedWarnings)
 		})
@@ -32,21 +34,13 @@ export async function testRule ({ description, rule, plugin, code, expectedWarni
 }
 
 /**
- * Retrieves specific rule configuration for stylelint.
+ * Get the rule config from the `stylelint.config.js` file.
  *
- * @param {string} ruleName - Name of the rule to get config for.
- * @param {string} [pluginName] - Optional name of the plugin.
- * @return {Promise<Object|undefined>} Promise that resolves to an object with plugins and rules if ruleName is provided, otherwise undefined.
+ * @param {string} rule - The name of the rule.
+ * @returns {any} The config of the specified rule.
  */
-async function getConfig (ruleName, pluginName) {
-	if (!ruleName) return
+async function getRuleConfig (rule) {
+	let configObject = await import(`../stylelint.config.js`).then((m) => m.default)
 
-	let configObject = JSON.parse(await readFile(`./.stylelintrc.json`, { encoding: `utf8` }))
-
-	return {
-		plugins: pluginName ? [pluginName] : undefined,
-		rules: {
-			[ruleName]: configObject.rules[ruleName],
-		},
-	}
+	return configObject.rules[rule]
 }
